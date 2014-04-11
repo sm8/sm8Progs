bb9898de5c0a71f49b59fb197192a7274e48a8bb
Steve M <sm8@bolton.ac.uk>
2014-04-11 16:59:58 +0100

    Hello World in TESTING branch added!

---
+++ testing/HW1a/HelloWorldTest1/main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+	cout << "Hello, World!\n\n";
+	cout << "This is for testing changes in the original project!\n\n";
+
+	cout << "This is from the TESTING branch version!\n\n";
+
+	system("pause");
+}
