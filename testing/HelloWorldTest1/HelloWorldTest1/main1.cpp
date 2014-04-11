4d908518a4a41268a723ca90f84d4256d7d6e899
Steve M <sm8@bolton.ac.uk>
2014-04-11 17:37:49 +0100

    Hello World 1 updated in testing

---
+++ testing/HelloWorldTest1/HelloWorldTest1/main1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+	cout << "Hello, World!\n\n";
+	cout << "This is for testing changes in the original project!\n\n";
+	cout << "!\n\n";
+	cout << "!\n\n";
+	cout << "This is for testing MERGES with testing branch!\n\n";
+	system("pause");
+}
