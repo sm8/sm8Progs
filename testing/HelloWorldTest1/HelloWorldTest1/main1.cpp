b6e02dde5469cb1155745dfad0fc01c42593f2e1
Steve M <sm8@bolton.ac.uk>
2014-04-11 17:45:31 +0100

    Add Hello world1 to testing branch

---
+++ testing/HelloWorldTest1/HelloWorldTest1/main1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+	cout << "Hello, World!\n\n";
+	cout << "This is for testing changes in the original project!\n\n";
+	cout << "!\n\n";
+	cout << "This is for testing MERGES!\n\n";
+	system("pause");
+}
